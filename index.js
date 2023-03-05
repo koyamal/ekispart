cc7c48300a72e6626e99b96a9df014b4f720f74a
koyamal <tanakamaruko1121n@gmail.com>
2023-03-05 20:43:47 +0900

    Make getStartEndTime

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const dotenv = require("dotenv");
 const url = dotenv.config().parsed.ekiUrl;
 const tgtText = dotenv.config().parsed.targetText;
 
+const getStartEndTime = function(timeText){
+    let startTime = "";
+    let endTime = "";
+
+    for(let i = 0; i < 5; i++){
+        startTime += timeText[i];
+        endTime += timeText[33 + i];
+    }
+    return {startTime, endTime};
+}
+
 const getDepatureTime = async function(){
     const body = await axios(url);
     const rowData = body.data;
@@ -12,14 +23,9 @@ const getDepatureTime = async function(){
     for(let i = 0; i < devidedData.length; i++){
         if(devidedData[i].includes(tgtText)){
             const timeText = devidedData[i + 3];
-            let startTime = "";
-            let endTime = "";
-            for(let j = 0; j < 5; j++){
-                startTime += timeText[j];
-                endTime += timeText[33 + j];
-            }
-            console.log(startTime);
-            console.log(endTime);
+            const seTime = getStartEndTime(timeText);
+            console.log(seTime.startTime);
+            console.log(seTime.endTime);
         }
     }
 }
