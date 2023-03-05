f61df9b3368a35d81b332429cbbe16dd4bceb78c
koyamal <tanakamaruko1121n@gmail.com>
2023-03-05 22:20:12 +0900

    Get year, month, day, hour, minute

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const urlCommonPart = dotenv.config().parsed.ekiUrl;
 const urlTimePart = '&yyyymm=202303&day=5&hour=21&minute10=4&minute1=9'
 const url = urlCommonPart + urlTimePart;
 
+const nowDate = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
+const dateSet = {}
+dateSet.year = nowDate.getFullYear().toString();
+
+(nowDate.getMonth() + 1).toString().length === 2 ? dateSet.month = (nowDate.getMonth() + 1).toString(): dateSet.month = '0' + (nowDate.getMonth() + 1);
+nowDate.getDate().toString().length === 2 ? dateSet.day = nowDate.getDate().toString(): dateSet.day = '0' + nowDate.getDate();
+nowDate.getHours().toString().length === 2 ? dateSet.hour = nowDate.getHours().toString(): dateSet.hour = '0' + nowDate.getHours();
+nowDate.getMinutes().toString().length === 2 ? dateSet.minute = nowDate.getMinutes().toString(): dateSet.minute = '0' + nowDate.getMinutes();
+
+console.log(dateSet);
+console.log(nowDate);
+
 const tgtText = dotenv.config().parsed.targetText;
 
 const getStartEndTime = function(timeText){
