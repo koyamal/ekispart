17c8d0c1808f2ce300886028d2f8b677db6862b7
koyamal <tanakamaruko1121n@gmail.com>
2023-03-05 22:25:54 +0900

    Search now date

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const axios = require("axios");
 const dotenv = require("dotenv");
 
-const urlCommonPart = dotenv.config().parsed.ekiUrl;
-const urlTimePart = '&yyyymm=202303&day=5&hour=21&minute10=4&minute1=9'
-const url = urlCommonPart + urlTimePart;
-
 const nowDate = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
 const dateSet = {}
-dateSet.year = nowDate.getFullYear().toString();
 
+dateSet.year = nowDate.getFullYear().toString();
 (nowDate.getMonth() + 1).toString().length === 2 ? dateSet.month = (nowDate.getMonth() + 1).toString(): dateSet.month = '0' + (nowDate.getMonth() + 1);
 nowDate.getDate().toString().length === 2 ? dateSet.day = nowDate.getDate().toString(): dateSet.day = '0' + nowDate.getDate();
 nowDate.getHours().toString().length === 2 ? dateSet.hour = nowDate.getHours().toString(): dateSet.hour = '0' + nowDate.getHours();
@@ -16,6 +12,10 @@ nowDate.getMinutes().toString().length === 2 ? dateSet.minute = nowDate.getMinut
 
 console.log(dateSet);
 console.log(nowDate);
+
+const urlCommonPart = dotenv.config().parsed.ekiUrl;
+const urlTimePart = `&yyyymm=${dateSet.year}${dateSet.month}&day=${dateSet.day}&hour=${dateSet.hour}&minute10=${dateSet.minute[0]}&minute1=${dateSet.minute[1]}`;
+const url = urlCommonPart + urlTimePart;
 
 const tgtText = dotenv.config().parsed.targetText;
 
