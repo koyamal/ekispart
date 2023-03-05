c8c78da4d7a28931f76d1b1a46bf243f921899b1
koyamal <tanakamaruko1121n@gmail.com>
2023-03-05 18:27:42 +0900

    Get body by axios

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const axios = require("axios");
+const dotenv = require("dotenv");
+
+const url = dotenv.config().parsed.ekiUrl;
+
+console.log(url);
+
+const getDepatureTime = async function(){
+    const body = await axios(url);
+    console.log(body.data);
+}
+
+getDepatureTime();
