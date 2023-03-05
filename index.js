fd313684e43feed9763f702a865b4cd2468533b6
koyamal <tanakamaruko1121n@gmail.com>
2023-03-05 18:47:11 +0900

    Get text having target value

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const axios = require("axios");
 const dotenv = require("dotenv");
 
 const url = dotenv.config().parsed.ekiUrl;
-
-console.log(url);
+const tgtText = dotenv.config().parsed.targetText;
 
 const getDepatureTime = async function(){
     const body = await axios(url);
-    const rowData = body.data
-    console.log(rowData);
-    console.log(typeof rowData);
+    const rowData = body.data;
+    const devidedData = rowData.split('\n');
+
+    for(let i = 0; i < devidedData.length; i++){
+        if(devidedData[i].includes(tgtText)){
+            console.log(devidedData[i]);
+        }
+    }
 }
 
 getDepatureTime();
