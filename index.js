e90ba9f29b7a7609111066a29177489d7fc9ad8b
koyamal <tanakamaruko1121n@gmail.com>
2023-03-07 20:44:45 +0900

    Change behavior num of dpt

--- index.js
+++ index.js
@@ -86,9 +86,21 @@ const init = async function(){
     let speakOutput;
     try{
         const dpt = await getDepatureTime();
+        // const dpt = [
+        //     { startTime: '20:43', endTime: '20:55' }
+        //   ]
         console.log('dpt: ', dpt);
+        speakOutput = `次のバスは${calDiffNowAndDpt(dpt[0], dateSet)}分後の${dpt[0].startTime}で、`;
+        for(let i = 1; i < dpt.length; i++){
+            speakOutput += `その次は${calDiffNowAndDpt(dpt[i], dateSet)}分後の${dpt[i].startTime}で`;
+        }
+        speakOutput += 'す。'
         
-        speakOutput = `次のバスは${calDiffNowAndDpt(dpt[0], dateSet)}分後の${dpt[0].startTime}で、その次は${calDiffNowAndDpt(dpt[1], dateSet)}分後の${dpt[1].startTime}です。`;
+        dpt[1]? 
+            speakOutput = `次のバスは${calDiffNowAndDpt(dpt[0], dateSet)}分後の${dpt[0].startTime}で、その次は${calDiffNowAndDpt(dpt[1], dateSet)}分後の${dpt[1].startTime}です。`:
+            speakOutput = `次のバスは${calDiffNowAndDpt(dpt[0], dateSet)}分後の${dpt[0].startTime}です。`
+        
+        //speakOutput = `次のバスは${calDiffNowAndDpt(dpt[0], dateSet)}分後の${dpt[0].startTime}で、その次は${calDiffNowAndDpt(dpt[1], dateSet)}分後の${dpt[1].startTime}です。`;
 
     }catch(e){
         speakOutput = e.message;
